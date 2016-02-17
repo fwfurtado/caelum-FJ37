82f6705dc8cef177aa4a6c7c999f71c6f1711268
Fernando Willian <feh.wilinando@gmail.com>
2016-02-17 10:29:48 -0200

    Configurado classe Book para ser uma entidade no banco de dados

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Book.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Book.java
@@ -1,11 +1,19 @@
 package br.com.casadocodigo.loja.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 /**
  * Created by Nando on 15/02/16.
  */
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
     private String description;

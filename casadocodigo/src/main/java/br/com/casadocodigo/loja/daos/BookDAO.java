5c5f7328c7e6d43979cac47f0a34656852248415
Fernando Willian <feh.wilinando@gmail.com>
2016-02-17 22:21:15 -0200

    Adicionado método no BookDAO  para retornar a Lista de Livros

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/BookDAO.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/BookDAO.java
@@ -5,7 +5,7 @@ import br.com.casadocodigo.loja.models.Book;
 import javax.enterprise.context.Dependent;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * Created by Nando on 16/02/16.
@@ -16,9 +16,13 @@ public class BookDAO {
     @PersistenceContext
     private EntityManager manager;
 
-    @Transactional
+
     public void save(Book book){
         manager.persist(book);
+    }
+
+    public List<Book> list(){
+        return manager.createQuery("select distinct b from Book b join fetch b.authors", Book.class).getResultList();
     }
 
 }

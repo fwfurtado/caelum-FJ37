4057f57a0aef79104518b39f973ab865e9ef25f5
Fernando Willian <feh.wilinando@gmail.com>
2016-02-25 22:37:17 -0300

    Criado um dao para persistir uma compra

---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/CheckoutDAO.java
@@ -0,0 +1,19 @@
+package br.com.casadocodigo.loja.daos;
+
+import br.com.casadocodigo.loja.models.Checkout;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Created by Nando on 25/02/16.
+ */
+public class CheckoutDAO {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    public void save(Checkout checkout) {
+        manager.persist(checkout);
+    }
+}

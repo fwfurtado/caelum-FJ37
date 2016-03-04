c44f580d58da6e3cb606d06f0698c2e15a5e01e7
Fernando Willian <feh.wilinando@gmail.com>
2016-03-04 19:01:40 -0300

    Adicionado configuração para Queue

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/configuration/ConfigureJMSDestinations.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/configuration/ConfigureJMSDestinations.java
@@ -1,13 +1,23 @@
 package br.com.casadocodigo.loja.configuration;
 
 import javax.jms.JMSDestinationDefinition;
+import javax.jms.JMSDestinationDefinitions;
 
 /**
  * Created by Nando on 01/03/16.
  */
+@JMSDestinationDefinitions({
+        @JMSDestinationDefinition(
+                                    name = ConfigureJMSDestinations.CHECKOUT_TOPIC_JNDI,
+                                    interfaceName = "javax.jms.Topic"
+        ),
+        @JMSDestinationDefinition(
+                                    name = ConfigureJMSDestinations.PAYMENT_INTEGRATION_QUEUE_JNDI,
+                                    interfaceName = "javax.jms.Queue"
+        )
 
-@JMSDestinationDefinition(
-                            name = "java:/jms/topics/checkoutTopic",
-                            interfaceName = "javax.jms.Topic"
-                        )
-public class ConfigureJMSDestinations {}
+})
+public class ConfigureJMSDestinations {
+        public static final String CHECKOUT_TOPIC_JNDI = "java:/jms/topics/checkoutTopic";
+        public static final String PAYMENT_INTEGRATION_QUEUE_JNDI = "java:/jms/queues/paymentQueue";
+}

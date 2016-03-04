5a81d4dd68cd8ab23ea0c859440a5a2979e573d9
Fernando Willian <feh.wilinando@gmail.com>
2016-03-04 14:30:02 -0300

    Adicionado componentes do jsf na página de login e criado um MB para efetuar o login

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/managedbeans/login/LoginBean.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/managedbeans/login/LoginBean.java
@@ -1,10 +1,10 @@
 package br.com.casadocodigo.loja.managedbeans.login;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
-import javax.servlet.ServletRegistration;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 /**
  * Created by Nando on 03/03/16.
@@ -15,10 +15,48 @@ public class LoginBean {
     @Inject
     private HttpServletRequest request;
 
-    public void login(){
-        Map<String, ? extends ServletRegistration> servletRegistrations = request.getServletContext().getServletRegistrations();
+    private String username;
+    private String password;
 
-        System.out.println(servletRegistrations);
+
+    @PostConstruct
+    public void load(){
+
+        System.out.println(request.getRequestURL());
+        System.out.println(request.getRequestURI());
+
+        try {
+            request.logout();
+        } catch (ServletException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
+    public void  login(){
+
+        try {
+            request.login(username,password);
+        } catch (ServletException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
